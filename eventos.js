0a23bf10d106207b2f3ffdddf7643b85f65631b5
DiegoArley <141291733+DiegoArley@users.noreply.github.com>
2023-08-30 00:39:45 -0300

    Sitio web creado

---
+++ eventos.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const saludarButton = document.getElementById("saludarButton");
+    const saludarDiv = document.getElementById("saludarDiv");
+  
+    saludarButton.addEventListener("click", function () {
+      alert("Hola!");
+    });
+  
+    saludarDiv.addEventListener("click", function () {
+      alert("Hola! Soy el div");
+    });
+  });
